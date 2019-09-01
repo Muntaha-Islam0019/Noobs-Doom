955899e0e0a939b7e323fb834c4cbe0dbdfc1eb9
Muntaha Islam <muntaha.islam0019@gmail.com>
2019-09-02 01:23:41 +0600

    Added Collision Detection

--- Assets/Scripts/FPSInput.cs
+++ Assets/Scripts/FPSInput.cs
@@ -11,10 +11,13 @@ public class FPSInput : MonoBehaviour
     
     public float speed = 6.0f;
     
+    // Initializing the CharacterController component.
+    private CharacterController _charController;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        _charController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -28,6 +31,23 @@ public class FPSInput : MonoBehaviour
            between frames. For instance, 30FPS monitors have 1/30 second as deltaTime. That's how it's
            scaled.
         */
-        transform.Translate(deltaX * Time.deltaTime, 0, deltaZ * Time.deltaTime);
+        // transform.Translate(deltaX * Time.deltaTime, 0, deltaZ * Time.deltaTime);
+        
+        /* As, directly changing the objects transform does not apply collision detection,
+           we're gonna use _charController. 
+        */
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        
+        /* Limiting diagonal movement same as axis movement, or, diagonal movement would have
+           greater magnitude.
+        */
+        movement = Vector3.ClampMagnitude(movement, speed);
+        
+        // Scaling by deltaTime.
+        movement *= Time.deltaTime;
+        
+        // Making local movement, global.
+        movement = transform.TransformDirection(movement);
+        _charController.Move(movement);
     }
 }
