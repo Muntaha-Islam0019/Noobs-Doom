a698d0285b028fa043388e30fa04a83bdf6709d9
Muntaha Islam <muntaha.islam0019@gmail.com>
2019-09-02 02:57:48 +0600

    Added gravity and Polishing

--- Assets/Scripts/FPSInput.cs
+++ Assets/Scripts/FPSInput.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// This ensures the components needed are attached.
+[RequireComponent(typeof(CharacterController))]
+
+// This adds the script to the ordered position.
+[AddComponentMenu("Control Script/FPS Input")]
+
 public class FPSInput : MonoBehaviour
 {
     
@@ -10,6 +16,7 @@ public class FPSInput : MonoBehaviour
     */
     
     public float speed = 6.0f;
+    public float gravity = -9.8f;
     
     // Initializing the CharacterController component.
     private CharacterController _charController;
@@ -42,6 +49,14 @@ public class FPSInput : MonoBehaviour
            greater magnitude.
         */
         movement = Vector3.ClampMagnitude(movement, speed);
+        
+        // Adding gravity(-9.8) to y instead of 0.
+        movement.y = gravity;
+        
+        /* Also, now the MouseLook on player would be just horizontal, so that, the gravity can't be tilted
+           (as the player could tilt). Additionally, now the camera has a MouseLook component too.
+           There, it'd just vertical. Therefore, even though, the camera could rotate vertically
+           independently, it moves horizontally with the player(as the player is the parent of the camera).*/
         
         // Scaling by deltaTime.
         movement *= Time.deltaTime;
