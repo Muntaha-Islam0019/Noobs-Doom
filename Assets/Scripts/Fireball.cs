79f2ed7fb91d6e2b2d909a6e5d479252053a5b97
Muntaha Islam <muntaha.islam0019@gmail.com>
2019-12-16 14:17:58 +0600

    Added shooting ability to enemy

---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public int damage = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+
+    // This function is called when the object is collided with an object.
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerCharacter player = other.GetComponent<PlayerCharacter>();
+        if (player != null)
+        {
+            Debug.Log("Hit!");
+        }
+        
+        Destroy(this.gameObject);
+    }
+}
--- Assets/Scripts/WanderingAI.cs
+++ Assets/Scripts/WanderingAI.cs
@@ -1,51 +1,63 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class WanderingAI : MonoBehaviour
 {
-    public float speed = 3.0f;
-    
     // Checking if the object is alive or not.
     private bool _alive;
-    
+    private GameObject _fireball;
+
+    [SerializeField] private GameObject fireballPrefab;
+
     // How far the object will stay from an object.
     public float obstacleRange = 5.0f;
 
+    public float speed = 3.0f;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         _alive = true;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         // Update, only if the object is alive.
         if (_alive)
         {
             transform.Translate(0, 0, speed * Time.deltaTime);
-        
-            // Generating a ray to forward of the object.
-            Ray ray = new Ray(transform.position, transform.forward);
 
+            // Generating a ray to forward of the object.
+            var ray = new Ray(transform.position, transform.forward);
             RaycastHit hit;
-        
+
             /*
              * Casting a ray consisting an area of sphere, as, this will help the object to find out if there
              * is an object in front of it. ScreenPointToRay could be used, though, then it'd cause problem
              * for certain objects.
              */
-            
+
             if (Physics.SphereCast(ray, 0.75f, out hit))
             {
-                if (hit.distance < obstacleRange)
-                { 
+                var hitObject = hit.transform.gameObject;
+                if (hitObject.GetComponent<PlayerCharacter>())
+                {
+                    if (_fireball == null)
+                    {
+                        _fireball = Instantiate(fireballPrefab);
+                        _fireball.transform.position = transform.TransformPoint(Vector3.forward);
+
+                        // Keep rotating in the same direction.
+                        _fireball.transform.rotation = transform.rotation;
+                    }
+                }
+                else if (hit.distance < obstacleRange)
+                {
                     /*
                      * If the object finds another object in front of it, it'd move towards
                      * a random angle.
                      */
-                    
+
                     float angle = Random.Range(-110, 110);
                     transform.Rotate(0, angle, 0);
                 }
@@ -53,7 +65,7 @@ public class WanderingAI : MonoBehaviour
         }
     }
 
-    public void setLife (bool life)
+    public void setLife(bool life)
     {
         _alive = life;
     }
